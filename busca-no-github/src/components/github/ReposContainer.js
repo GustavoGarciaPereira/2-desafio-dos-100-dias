086c9b3a267a0fafcab8b6b6ad726216a0d7a06f
gustavo <gusgurtavo@gmail.com>
2019-01-16 21:50:17 -0200

    dia 7 do desafio

--- busca-no-github/src/components/github/ReposContainer.js
+++ busca-no-github/src/components/github/ReposContainer.js
@@ -1,21 +1,43 @@
 import React, {Component} from 'react'
 import {fetchRepos} from '../../service/repos-api'
+import ReposList from './ReposList'
 
 class ReposContainer extends Component{
     constructor(props){
         super(props)
         this.state = {
-            repos: []
+            repos: [],
+            username:''
         }
+        this.changeHandler = this.changeHandler.bind(this)
+        this.submitHandler = this.submitHandler.bind(this)
     }
 
-    componentDidMount(){
-        fetchRepos('GustavoGarciaPereira').then(res => window.console.log(res.data))
+    changeHandler(ev){
+        
+        this.setState({username: ev.target.value})
+    }
+    submitHandler(ev){
+        ev.preventDefault()
+
+        fetchRepos(this.state.username).then(res => this.setState({repos: res.data}))
     }
 
     render(){
         return(
-            <h1>Respos Container</h1>
+            <div>
+                <h1>Repos Container</h1>
+                <form action="#" onSubmit={this.submitHandler}>
+                    <input
+                    onChange={this.changeHandler}
+                    style={{width:'300pxw'}}
+                    type="search"
+                    placeholder="Informe o usuário ENTER"
+                    
+                    />
+                </form>
+                <ReposList repos={this.state.repos}></ReposList>
+            </div>
         )
     }
 }
